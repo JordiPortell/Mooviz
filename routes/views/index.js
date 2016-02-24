9b18106555c3d2b307a9abb8622f7fcb796863ca
Alexandre Lavigne <apj.lavigne@gmail.com>
2016-02-24 12:09:28 +0100

    Update movies

--- routes/views/index.js
+++ routes/views/index.js
@@ -14,7 +14,7 @@ exports = module.exports = function(req, res) {
 
 		console.log(req.body.text);
 		
-		omdb.search('Star Wars', function(err, movies) {
+		omdb.search('star', function(err, movies) {
 			if(err) {
 				return console.error(err);
 			}
@@ -22,24 +22,42 @@ exports = module.exports = function(req, res) {
 			if(movies.length < 1) {
 				return console.log('No movies were found!');
 			}
-
+			
 			movies.forEach(function(movie) {
 				console.log('%s (%d)', movie.title, movie.year);
+				var Nmovie = keystone.list('movie');
+
+				var newMovie = new Nmovie.model({
+					Title: movie.title,
+					Year: movie.year,
+					Rated: movie.rated,
+					Released: movie.released,
+					Runtime: movie.runtime,
+					Genre: movie.genre,
+					Director: movie.director,
+					Writer: movie.writer,
+					Actors: movie.actors,
+					Plot: movie.plot,
+					Language: movie.language,
+					Country: movie.country,
+					Awards: movie.awards,
+					Poster: movie.poster,
+					Metascore: movie.metascore,
+					imdbRating: movie.imdbRating,
+					imdbVotes: movie.imdbVotes,
+					imdbID: movie.imdbID,
+					Type: movie.type,
+					Response: movie.response
+				});
+
+
+				newMovie.save(function(err) {
+					// post has been saved	
+				});
 			});
 		});
 
-		omdb.get({ title: 'Star Wars', year: 1999 }, true, function(err, movie) {
-			if(err) {
-				return console.error(err);
-			}
-			if(!movie) {
-				return console.log('Movie not found!');
-			}
-			console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-			console.log(movie.plot);
-		});
-		
-		//document.getElementById("moviesearch").appendChild('<br>TEST');
+	
 
 	});
 
