86385c6e9e20684f4dcf397052cf6385868665db
jordi portell <portelljordi@gmail.com>
2016-02-24 11:23:19 +0100

    Intégration de la collection movie

--- models/movie.js
+++ models/movie.js
@@ -1,31 +1,42 @@
 /**
  * Created by jordi_2 on 06/02/2016.
  */
-/*var keystone = require('keystone');
+var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 var Movie = new keystone.List('Movie', {
-	map: { name: 'title' },
-	autokey: { path: 'slug', from: 'title', unique: true }
+	map: { name: 'Title' },
+	autokey: { path: 'slug', from: 'Title', unique: true }
 });
 
 Movie.add(
 {
-	imdburl:{type:String,required :true},
-	title: { type: String, required: true },
-	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	image: { type: Types.CloudinaryImage },
-	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 }
-	}
+ Title:{type:String,required :true},
+ Year:{type:Types.Number},
+ Rated: {type:Types.Number},
+ Released: {type:Types.Date},
+ Runtime:{type:String},
+ Genre:{type:String},
+ Director:{type:String},
+ Writer:{type:String},
+ Actors:{type:String},
+ Plot:{type:String},
+ Language:{type:String},
+ Country:{type:String},
+ Awards:{type:String},
+ Poster:{type: Types.CloudinaryImage},
+ Metascore:{type:Types.Number},
+ imdbRating:{type:Types.Number},
+ imdbVotes:{type:Types.Number},
+ imdbID:{type:Types.Number},
+ Type:{type:String},
+ Response:{type:Types.Boolean}
 });
 
 Movie.schema.virtual('content.full').get(function() {
 	return this.content.extended || this.content.brief;
 });
 
-Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
-Post.register();
-*/
+//Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
+Movie.register();
+
