3fb773aed1516ee6bbc171a13714f022f7644eb5
jordi portell <portelljordi@gmail.com>
2016-06-22 01:10:18 +0200

    amélioration de l'affichage et début de reporting

--- routes/views/reporting.js
+++ routes/views/reporting.js
@@ -11,25 +11,43 @@ exports = module.exports = function(req, res) {
 	locals.data = {
 		Genre: []
 	};
-
-	var Genre = [];
+	
 
 // Set locals
 	locals.section = 'movie';
-
+	
 	//récupération de la liste des genres dans un tableau
-	movie.model.find().distinct('Genre')
+	movie.model.aggregate({"$group" : {_id:"$Genre", count:{$sum:1}}})
 		.exec()
 		.then(function (movies) { //first promise fulfilled
-			for (i = 0; i < movies.length; i++) {
-				var data_array = movies[i].replace(/\s/g, '').split(',');
-				for (j = 0; j < data_array.length; j++) {
-					//console.log(locals.data.Genre.indexOf(data_array[j]));
-					if (locals.data.Genre.indexOf(data_array[j]) == -1 && data_array[j] != '') {
-						locals.data.Genre.push(data_array[j]);
+				console.log(movies);
+				locals.data.Genre=movies;
+				movie.model.aggregate({"$group" : {_id:"$Title",count:{$sum:1}}})
+			.exec()
+			.then(function (movies) { //first promise fulfilled
+				console.log(movies);
+							view.render('reporting');
+						}, function (err) { //first promise rejected
+							throw err;
+						}).then(function (result) {
+							console.log(result);
+							//second promise fulfilled
+							//do something with final results
+						}, function (err){ //something happened
+							//catch the error, it can be thrown by any promise in the chain
+							console.log(err);
+						});
+						
+			
+				/*for (i = 0; i < movies.length; i++) {
+					var data_array = movies[i].replace(/\s/g, '').split(',');
+					for (j = 0; j < data_array.length; j++) {
+						//console.log(locals.data.Genre.indexOf(data_array[j]));
+						if (locals.data.Genre.indexOf(data_array[j]) == -1 && data_array[j] != '') {
+							locals.data.Genre.push(data_array[j]);
+						}
 					}
-				}
-			}
+			}*/
 			}, function (err) { //first promise rejected
 				throw err;
 			}).then(function (result) {
@@ -40,8 +58,8 @@ exports = module.exports = function(req, res) {
 				//catch the error, it can be thrown by any promise in the chain
 				console.log(err);
 			});
-	
-	console.log(locals.data.Genre);
+
+	/*console.log(locals.data.Genre);
 	
 	for (k = 0; k < locals.data.Genre.length; k++) {
 		movie.model.count({"Genre": {$regex: locals.data.Genre[k]}})
@@ -57,7 +75,7 @@ exports = module.exports = function(req, res) {
 					//catch the error, it can be thrown by any promise in the chain
 					console.log(err);
 				});
-	}
+	}*/
 };	
 
 	
